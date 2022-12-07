2dcacc3754276cafe559ea281c4e51751180a572
chaaanhee <cksgml8157@gmail.com>
2022-12-07 21:17:13 +0900

    두수의 차

---
+++ lv0/src/lv0/두_수의차.java
@@ -0,0 +1,14 @@
+package lv0;
+public class 두_수의차 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(solution(2,4));
+	}
+
+	static int solution(int num1, int num2) {
+		int answer = num1 - num2;
+		return answer;
+	}
+
+}
---
+++ lv0/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chanhee
+ *
+ */
+module Lv0 {
+}
